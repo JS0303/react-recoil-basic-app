2ad72f13a5b68385ec8a05668a1194ffbb3b2650
js0303 <wlstjr0303@gmail.com>
2023-03-08 15:10:05 +0900

    Recoil Basic App

---
+++ src/components/CharacterCount.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { charCountState } from "../App";
+
+const CharacterCount = () => {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+};
+
+export default CharacterCount;
